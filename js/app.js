eaf2e02a7cfe05a843bff39defb85c125b5d9266
Ezra <ezralangat88@gmail.com>
2021-12-29 16:45:55 +0300

    1st commit

---
+++ js/app.js
@@ -0,0 +1,35 @@
+//All selectors start with document
+let oddLinks = document.querySelectorAll("#primary a:nth-child(odd)");
+
+oddLinks.forEach((oddlink) => {
+	oddlink.style.backgroundColor = "red";
+	oddlink.style.color = "white";
+});
+
+let addToCartBtns = document.querySelectorAll(".price");
+addToCartBtns.forEach((btn) => {
+	btn.textContent = "2000ksh";
+});
+console.log(addToCartBtns);
+//My Practice
+let heading = document.getElementById('heading');
+
+heading.style.background = 'black';
+heading.style.color = 'white';
+heading.textContent = 'Scintilating Courses!';
+heading.innerText = 'New Courses';
+
+//Query Selector - select id or class - systax similar to css 
+const learningHeading = document.querySelector('#learn');
+
+const tagline = document.querySelector('.tagline');
+const newText = tagline.textContent.replace('$15','$10');
+
+tagline.textContent = newText;
+
+//Selecting by tag
+let heading2 = document.querySelector('h2');
+
+console.log(heading2);
+//End of My Practice
+
