11414f973a890238fd2f447e49d2c40f4dc8cdee
Ezra <ezralangat88@gmail.com>
2021-12-30 12:22:54 +0300

    Modules..

--- js/app.js
+++ js/app.js
@@ -41,7 +41,10 @@ link = document.querySelector('#primary a:nth-child(2)')
 
 console.log(link );
 
+// ES6 stands for ECMAScript 6.
 
+// ECMAScript was created to standardize JavaScript
+//--Arrow functions--
 //Arrow functions allow us to write shorter function syntax:
 
 hello = function() {
@@ -82,5 +85,57 @@ function calculate(a, b) {
   }
   
   const [add, subtract, multiply, divide] = calculate(4, 7);
+
+
+// --CONST-- does not define a constant value. It defines a constant reference to a value.
+
+// Because of this you can NOT:
+// Reassign a constant value, array and object
+
+// But you CAN:
+// Change the elements of constant array
+// Change the properties of constant object
+
+//-- MODULES--
+// They allow you to break up your code into separate files.
+// ES Modules rely on the import and export statements.
+//Export - There are two types of exports: Named and Default.
+
+//Named Exports
+//You can create named exports two ways:
+
+//In-line individually in person.js
+export const name = "Jesse"
+export const age = "40"
+
+// All at once at the bottom:
+const name = "Jesse"
+const age = "40"
+
+export { name, age }
+
+// Default Exports
+// You can only have one default export in a file.
+const message = () => {
+	const name = "Jesse";
+	const age = "40";
+	return name + ' is ' + age + 'years old.';
+  };
+  
+  export default message;
+
+// Import
+// You can import modules into a file in two ways, 
+//based on if they are named exports or default exports.
+  
+// Named exports must be destructured using curly braces. 
+//Default exports do not.
+
+//Import named exports from the file person.js:
+import { name, age } from "./person.js";
+//Import a default export from the file message.js:
+import message from "./message.js";
+
+
 //End of My Practice
 
